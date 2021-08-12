1b7bc62d33a74dc3cb61b9a4d5483580562cceb0
Bruno Nascimento <geeknection@gmail.com>
2021-08-11 21:25:29 -0300

    Project

--- app/services/mongodb/index.ts
+++
@@ -1,8 +0,0 @@
-import TransactionsSchema from "./schemas/transaction";
-import UsersSchema from "./schemas/user";
-
-namespace Schema {
-    export const Users = UsersSchema;
-    export const Transactions = TransactionsSchema;
-}
-export default Schema;
